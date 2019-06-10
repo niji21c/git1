158e86984bb45a6d3342e980dc9de340f6a4efd3
dhkim <dhkim@218.38.137.27>
2019-06-10 17:57:25 +0900

    b2

--- src/main/java/com/test/lettuce/controller/LettuceController.java
+++ src/main/java/com/test/lettuce/controller/LettuceController.java
@@ -48,6 +48,7 @@ public class LettuceController {
 
     @GetMapping("/save")
     public String save(){
+		System.out.println("b2");
         String randomId = createId();
         LocalDateTime now = LocalDateTime.now();
 
